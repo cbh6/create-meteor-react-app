8faedf017edadbc2f6bc470cf96dcfc29ef20ac2
Cristian <cristian@digio.es>
2018-09-24 14:49:21 +0200

    Add Fontawesome React replacing SemantiUi Icon package

--- imports/ui/pages/NotFoundPage.js
+++ imports/ui/pages/NotFoundPage.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import {
-  Container, Header, Icon, Grid,
-} from 'semantic-ui-react';
+import { Container, Header, Grid } from 'semantic-ui-react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 
 const NotFoundPage = () => (
   <Container>
-    <Header as="h2" textAlign="center">
-      <Icon name="warning sign" />
-      404 Not Found
+    <Header as="h2">
+      {/* <Icon name="warning sign" /> */}
+      {/* <Icon loading name="spinner" /> */}
+      <FontAwesomeIcon icon={faExclamationTriangle} size="2x" />
+      {' '}
+404 Not Found
     </Header>
     <Grid columns={1}>
       <Grid.Column>Sorry, the page you are trying to view does not exist!</Grid.Column>
