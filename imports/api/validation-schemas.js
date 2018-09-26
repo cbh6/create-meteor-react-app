665281e2429c9e2c9258b21646f249f5afab0315
Cristian <cristian@digio.es>
2018-09-26 13:23:45 +0200

    Add formik and yup to Login page form

---
+++ imports/api/validation-schemas.js
@@ -0,0 +1,12 @@
+import * as Yup from 'yup';
+
+export const userValidationSchema = Yup.object().shape({
+  email: Yup.string()
+    .email('Invalid email address')
+    .required('Email is required'),
+  password: Yup.string()
+    .min(6, 'Password must be 6 characters or longer')
+    .required('Password is required'),
+});
+
+export const hola = '';
--- imports/ui/pages/LoginPage.js
+++ imports/ui/pages/LoginPage.js
@@ -1,106 +1,112 @@
-import React, { Component } from 'react';
+import React, { Fragment } from 'react';
 import { withRouter, Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { Formik } from 'formik';
 import {
   Button, Form, Message, Header, Grid, Segment,
 } from 'semantic-ui-react';
-import Validators from '../../api/validators';
+import { userValidationSchema } from '../../api/validation-schemas';
 
-class LoginPage extends Component {
-  state = { email: '', password: '', error: '' };
+// const loginValidationSchema = Yup.object().shape({
+//   email: Yup.string()
+//     .email('Invalid email address')
+//     .required('Email is required!'),
+//   password: Yup.string()
+//     .min(9, 'Password must be 9 characters or longer')
+//     .required('Password is required'),
+// });
 
-  componentDidMount() {
-    throw new Error('I Crashed !!');
-  }
-
-  handleChange = (e, { name, value }) => this.setState({ [name]: value });
-
-  handleSubmit = () => {
-    const { email, password } = this.state;
-    const { history } = this.props;
-
-    if (!this.validForm()) return false;
-    Meteor.loginWithPassword(email, password, (err) => {
-      if (err) {
-        this.setState({ error: err.reason });
-        return false;
-      }
-      Bert.alert('Logged in', 'success');
-      history.push('/');
-    });
-  };
-
-  validForm = () => {
-    const { email } = this.state;
-
-    if (!email) {
-      this.setState({ error: 'There are empty required fields' });
-      return false;
-    }
-
-    if (!Validators.validMailString(email)) {
-      this.setState({ error: 'Invalid email address' });
-      return false;
-    }
-
-    return true;
-  };
-
-  render() {
-    const { error, email, password } = this.state;
-    return (
-      <Grid centered columns={1}>
-        <Grid.Column className="centered-form">
-          <Header textAlign="center" as="h3">
-            Please Sign in
-          </Header>
-          <Segment>
-            <Message hidden={!error} color="red">
-              {error}
-            </Message>
-            <Form>
-              <Form.Input
-                onChange={this.handleChange}
-                value={email}
-                name="email"
-                fluid
-                required
-                label="Email"
-                type="Email"
-                placeholder="Email"
-              />
-              <Form.Input
-                onChange={this.handleChange}
-                value={password}
-                name="password"
-                fluid
-                required
-                label="Password"
-                type="Password"
-                placeholder="Password"
-              />
-              <Button fluid color="black" onClick={this.handleSubmit} type="submit">
-                Sign in
-              </Button>
-            </Form>
-          </Segment>
-          <p className="login-subtitle">
-            Not a member?
-            {' '}
-            <Link color="black" to="/register">
-              Sign up here
-            </Link>
-          </p>
-          <p className="login-subtitle">
-            Forgot password?
-            {' '}
-            <Link to="/forgot-password">Click here</Link>
-          </p>
-        </Grid.Column>
-      </Grid>
-    );
-  }
-}
+const LoginPage = props => (
+  <Grid centered columns={1}>
+    <Grid.Column className="centered-form">
+      <Header textAlign="center" as="h3">
+        Please Sign in
+      </Header>
+      <Segment>
+        <Formik
+          validationSchema={userValidationSchema}
+          initialValues={{ email: '', password: '' }}
+          onSubmit={(values, { setSubmitting }) => {
+            const { history } = props;
+            const { email, password } = values;
+            Meteor.loginWithPassword(email, password, (err) => {
+              if (err) {
+                Bert.alert(err.reason, 'danger');
+                return false;
+              }
+              Bert.alert('Logged in', 'success');
+              setSubmitting(false);
+              history.push('/');
+            });
+          }}
+          render={({
+            values,
+            touched,
+            errors,
+            handleSubmit,
+            handleChange,
+            handleBlur,
+            isSubmitting,
+          }) => (
+            <Fragment>
+              <Message
+                hidden={!Object.keys(errors).length || !Object.keys(touched).length}
+                color="red"
+              >
+                {Object.keys(errors).map(key => touched[key] && <p key={key}>{errors[key]}</p>)}
+              </Message>
+              <Form onSubmit={handleSubmit}>
+                <Form.Input
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.email}
+                  name="email"
+                  fluid
+                  required
+                  label="Email"
+                  type="Email"
+                  placeholder="Email"
+                />
+                <Form.Input
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.password}
+                  name="password"
+                  fluid
+                  required
+                  label="Password"
+                  type="Password"
+                  placeholder="Password"
+                />
+                <Button
+                  fluid
+                  color="black"
+                  onClick={handleSubmit}
+                  disabled={isSubmitting}
+                  type="submit"
+                >
+                  Sign in
+                </Button>
+              </Form>
+            </Fragment>
+          )}
+        />
+      </Segment>
+      <p className="login-subtitle">
+        Not a member?
+        {' '}
+        <Link color="black" to="/register">
+          Sign up here
+        </Link>
+      </p>
+      <p className="login-subtitle">
+        Forgot password?
+        {' '}
+        <Link to="/forgot-password">Click here</Link>
+      </p>
+    </Grid.Column>
+  </Grid>
+);
 
 LoginPage.propTypes = {
   history: PropTypes.object.isRequired,
